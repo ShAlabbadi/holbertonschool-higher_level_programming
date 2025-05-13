fb64fdad93bfd2011d1b12d83b3be4476e71b95a
ShAlabbadi <10708@holbertonstudents.com>
2025-05-13 12:32:06 +0000

    8. More returns!

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    
     length = len(sentence)
-    first = sentence[0]
+
+    if len(sentence) != 0:
+        first = sentence[0]
+    else:
+        first = 'None'
+
     return length, first
