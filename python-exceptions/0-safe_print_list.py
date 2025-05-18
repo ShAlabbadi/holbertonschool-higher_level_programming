7edf0ff61a84c41d86e81ab5b3d2855f8df32c79
ShAlabbadi <10708@holbertonstudents.com>
2025-05-18 16:54:55 +0000

    0. Safe list printing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    start = 0
+    count = 0
     for i in range(x):
         try:
             print(my_list[i], end="")
-            start += 1
+            count += 1
         except IndexError:
             break
     print()
-    return start
+    return count
