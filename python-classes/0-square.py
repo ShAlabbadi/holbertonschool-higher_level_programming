7f8ff3d003fd2d0ba1ffb6c5a011da4c74240461
ShAlabbadi <10708@holbertonstudents.com>
2025-05-19 10:54:17 +0000

    0. My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,6 +2,6 @@
 """empty class Square that defines a square"""
 
 
-    class Square:
-        """empty class Square that defines a square"""
-        pass
+class Square:
+    """empty class Square that defines a square"""
+    pass
