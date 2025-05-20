5c4d9a266cd643623881deed70a7f33f521d4316
ShAlabbadi <10708@holbertonstudents.com>
2025-05-20 11:18:18 +0000

    3. Area of a square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
- """Defines a square"""
+""" class Square that defines a square """
 
 
 class Square:
-    """Defines a square"""
+    """Represent a square."""
     def __init__(self, size=0):
-        """Initialises the data"""
-        self.__size = size
-        if type(size) != int:
+
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
-        """Returns current square area"""
-        return self.__size**2
+        """returns the current square area"""
+        return self.__size * self.__size
