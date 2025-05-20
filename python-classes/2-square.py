a96cb69d0f60c18a1b2aad4c32cddb8dbfe655e9
ShAlabbadi <10708@holbertonstudents.com>
2025-05-20 10:18:52 +0000

    2. Size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -14,3 +14,53 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+~                                                                                                                                               
+-- INSERT --                                                                                                                  15,31         All
