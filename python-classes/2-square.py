c368754bbbcdebb328ff1d76f2e8461c71fa2b95
ShAlabbadi <10708@holbertonstudents.com>
2025-05-20 10:27:17 +0000

    2. Size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,66 +1,13 @@
 #!/usr/bin/python3
 """ class Square that defines a square """
 
-class Square:
-    """ class Square that defines a square """
-    def __init__(self, size=0):
-        """Initialize a new Square.
 
-        Args:
-            size (int): The size of the new square.
-        """
+class Square:
+
+    def __init__(self, size=0):
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
-~                                                                                                                                               
--- INSERT --                                                                                                                  15,31         All
