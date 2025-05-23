87b54cdc4d2084280083717420d1176c4efa93c2
ShAlabbadi <10708@holbertonstudents.com>
2025-05-23 15:06:07 +0000

    3. String representation

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -39,12 +39,15 @@ class Rectangle:
 
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
-            return ""
+            return 0
         else:
             return (self.__width * 2) + (self.__height * 2)
 
     def __str__(self):
-        rectangle_str = []
-        for i in range (self.__height):
-            rectangle_str.append("#" * self.__width)
-        return "\n".join(rectangle_str)
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            rectangle_str = []
+            for i in range (self.__height):
+                rectangle_str.append("#" * self.__width)
+            return "\n".join(rectangle_str)
