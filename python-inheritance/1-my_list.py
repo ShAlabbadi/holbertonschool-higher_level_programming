226b89c232c13a933d0612f230e41843449d24a7
ShAlabbadi <10708@holbertonstudents.com>
2025-05-27 12:31:45 +0000

    3

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""  a class MyList that inherits from list """
+"""a class MyList that inherits from list"""
 
 
 class MyList(list):
-    """ a class named MyList"""
+    """a class named MyList"""
 
     def print_sorted(self):
         """Print the list in sorted ascending order."""
         print(sorted(self))
 
-#if __name__ == "__main__":
-    #import doctest
-    #doctest.testfile("tests/1-my_list.txt")
+
+# if __name__ == "__main__":
+# import doctest
+# doctest.testfile("tests/1-my_list.txt")
