b33655a2c5d33a9de1cdfe76f81605303bbe4232
ShAlabbadi <10708@holbertonstudents.com>
2025-05-27 10:37:09 +0000

    1. My list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -9,6 +9,6 @@ class MyList(list):
         print(sorted(self))
         return sorted(self)
 
-    if __name__ == "__main__":
-        import doctest
-        doctest.testfile("tests/1-my_list.txt")
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/1-my_list.txt")
