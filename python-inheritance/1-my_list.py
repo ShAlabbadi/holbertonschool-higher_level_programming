d59dda7d46f991a340ea14bca8ba703592a696e4
ShAlabbadi <10708@holbertonstudents.com>
2025-05-27 13:04:15 +0000

    1-my_list.py

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a class MyList that inherits from list.
+"""
+
+
+class MyList(list):
+    """MyList class inherits from list and adds a print_sorted method."""
+    def print_sorted(self):
+        """Prints the list in ascending order
+        without modifying the original list."""
+        print(sorted(self))
+
+    def str(self):
+        """Returns the string representation of the list."""
+        return str(list(self))
