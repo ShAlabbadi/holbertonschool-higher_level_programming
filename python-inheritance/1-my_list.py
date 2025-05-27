2dbe710ae2aba39f1af8986ca609c6a3e8ac930b
ShAlabbadi <10708@holbertonstudents.com>
2025-05-27 10:44:11 +0000

    1. My list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,9 +6,7 @@ class MyList(list):
     """ a class named MyList"""
 
     def print_sorted(self):
-        sorted_list = sorted(self)
         print(sorted(self))
-        return sorted_list
 
 if __name__ == "__main__":
     import doctest
