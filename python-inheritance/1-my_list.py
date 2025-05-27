7796d9a2d43f395808c1bf8e715af8d0e26a79e5
ShAlabbadi <10708@holbertonstudents.com>
2025-05-27 12:58:23 +0000

    1. My list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -8,3 +8,7 @@ class MyList(list):
     def print_sorted(self):
         """Prints the list sorted in ascending order"""
         print(sorted(self))
+
+
+        def str(self):
+            return str(list(self))
