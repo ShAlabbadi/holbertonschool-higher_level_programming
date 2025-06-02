b67790da9dd2977469ff515565c85c1cdba8c58e
ShAlabbadi <10708@holbertonstudents.com>
2025-06-02 15:29:49 +0000

    0. Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """Prints the contents of a UTF8 text file"""
     with open(filename, encoding="utf-8") as myfile:
-        print(myfile.read())
+        print(myfile.read(), end="")
