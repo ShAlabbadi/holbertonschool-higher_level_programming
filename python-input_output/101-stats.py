3c0cd1079966c1e54461ba66fa185a7bf22f0aa9
ShAlabbadi <10708@holbertonstudents.com>
2025-06-02 22:40:06 +0000

    14. Log parsing

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,52 +1,37 @@
 #!/usr/bin/python3
-"""Module for parsing and displaying log statistics"""
+"""A script that reads stdin line by line and computes metrics"""
+
 
 import sys
 
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
-def print_stats(total_size, status_codes):
-    """Print the statistics"""
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-def main():
-    """Main function to process input and display stats"""
-    total_size = 0
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                    403: 0, 404: 0, 405: 0, 500: 0}
-    line_count = 0
+except Exception as err:
+    pass
 
-    try:
-        for line in sys.stdin:
-            line_count += 1
-            parts = line.split()
-
-            if len(parts) >= 7:
-                try:
-                    total_size += int(parts[-1])
-                except ValueError:
-                    pass
-
-                try:
-                    status_code = int(parts[-2])
-                    if status_code in status_codes:
-                        status_codes[status_code] += 1
-                except ValueError:
-                    pass
-
-            if line_count % 10 == 0:
-                print_stats(total_size, status_codes)
-
-        if line_count > 0:
-            print_stats(total_size, status_codes)
-
-    except KeyboardInterrupt:
-        print_stats(total_size, status_codes)
-        raise
-
-
-if __name__ == "__main__":
-    main()
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
