758cdfe894793884580040c6b52d88a0c6f0a7c9
ShAlabbadi <10708@holbertonstudents.com>
2025-07-13 16:44:00 +0000

    10. Factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (isNaN(n)) return 1;
+  n = parseInt(n);
+  if (n <= 1) return 1;
+  return n * factorial(n - 1);
+}
+
+const num = process.argv[2];
+console.log(factorial(num));
