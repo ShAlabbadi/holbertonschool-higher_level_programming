2b92d09479364e65981be7d48d6103f35ce8a0b7
ShAlabbadi <10708@holbertonstudents.com>
2025-07-14 08:07:00 +0000

    1. Click and turn red

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const redHeader = document.querySelector('#red_header');
+  const header = document.querySelector('header');
+
+  redHeader.addEventListener('click', function () {
+    header.style.color = '#FF0000';
+  });
+});
