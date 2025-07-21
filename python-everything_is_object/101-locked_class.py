5b4f4d23bbc0e6dca6014223e5245cba5b354482
ShAlabbadi <10708@holbertonstudents.com>
2025-07-21 11:43:24 +0000

    31. Low memory cost

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 class LockedClass:
-    """A class that only allows 'first_name' as an instance attribute"""
-    __slots__ = ['first_name']
+    __slots__ = ["first_name"]
 
-    def __setattr__(self, name, value):
-        if name != 'first_name':
-            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
-        super().__setattr__(name, value)
+    def __init__(self):
+        self.first_name = "first_name"
